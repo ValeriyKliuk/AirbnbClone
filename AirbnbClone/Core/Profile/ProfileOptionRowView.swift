7c6acb52cdfcee99125ce8097d480bb47b14297b
Valeriy Kliuk <valeriy.kliuk@gmail.com>
2023-11-14 12:47:10 -0500

    Created ProfileOptionRowView

---
+++ AirbnbClone/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileOptionRowView.swift
+//  AirbnbClone
+//
+//  Created by Valery on 2023-11-14.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    let imageName: String
+    let title: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                
+                Text(title)
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+            }
+            
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ProfileOptionRowView(imageName: "globe", title: "Hello, World!")
+}
--- AirbnbClone/Core/Profile/ProfileView.swift
+++ AirbnbClone/Core/Profile/ProfileView.swift
@@ -11,7 +11,17 @@ struct ProfileView: View {
     var body: some View {
         VStack {
             ProfileLoginView()
+            
+            
+            VStack(content: {
+                ProfileOptionRowView(imageName: "gear", title: "Settings")
+                ProfileOptionRowView(imageName: "gear", title: "Accessibility")
+                ProfileOptionRowView(imageName: "questionmark.circle", title: "Visit the help center")
+            })
+            .padding(.vertical)
         }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
